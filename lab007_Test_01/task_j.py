9c056380ab63d356ad32ffcbcfe1cb3b068975a2
Evgeniy Pasko <pasko.evg@gmail.com>
2020-04-14 19:16:34 +0300

    Решена задача task_j в лабораторной работе lab007

--- lab007_Test_01/task_j.py
+++ lab007_Test_01/task_j.py
@@ -9,7 +9,7 @@
 
 # На первой строке вводится число 0 <= N <= 100 - количество дней.
 # Далее на 3*N строках идут тройки чисел(каждое в новой строке):
-# 1 <= l i <= r i <= 10 9 и 0 <= c i <= 10 6 .
+# 1 <= l(i) <= r(i) <= 10^9 и 0 <= c(i) <= 10^6 .
 # Далее идет число 0 < M <= 100 - число интересующих Васю дощечек.
 # Далее на M строках номера дощечек.
 
@@ -21,18 +21,63 @@
 # out: 2 1
 
 
+class FenceInterval(object):
+    def __init__(self, start: int, end: int, color: int):
+        self.start = start
+        self.end = end
+        self.color = color
+
+    def __repr__(self):
+        return "FenceInterval({0}, {1}, {2})".format(self.start, self.end, self.color)
+
+    def stick_in_interval(self, number: int):
+        if number >= self.start and number <= self.end:
+            return True
+        else:
+            return False
+
+    def get_color(self):
+        return self.color
+
+
+class Fence(object):
+    def __init__(self):
+        self.intervals = []
+
+    def __repr__(self):
+        return "Fence. Intervals: {0}".format(self.intervals)
+
+    def add_interval(self, interval: FenceInterval):
+        self.intervals.insert(0, interval)
+
+    def get_stick_color(self, stick_number):
+        color = 0
+        # print("Ищем цвет палки номер {0}".format(stick_number))
+        for i in self.intervals:
+            # print("Смотрим интервал {0}".format(i))
+            if i.stick_in_interval(stick_number):
+                # print("Успех! Цвет {0}".format(i.get_color()))
+                color = i.get_color()
+                break
+            # else:
+            #     print("Не Успех")
+
+        return color
+
+
 if __name__ == '__main__':
+    fence = Fence()
 
-    day_count = int(input())
-    input_data = []
-    for i in range(day_count):
-        l, r, c = int(input()), int(input()), int(input())
-        input_data.append((l, r, c))
+    # day_count
+    for i in range(int(input())):
+        fence.add_interval(FenceInterval(int(input()), int(input()), int(input())))
 
-    stick_count = int(input())
-    stick_num = []
-    for i in range(stick_count):
-        stick_num.append(int(input()))
+    stick_color = []
+    for i in range(int(input())):
+        stick_color.append(str(fence.get_stick_color(int(input()))))
 
-    print('day count: {0}, data: {1}'.format(day_count, input_data))
-    print('stick count: {0}, stick num: {1}'.format(stick_count, stick_num))
+    # print('day count: {0}, data: {1}'.format(day_count, input_data))
+    # print('stick count: {0}, stick num: {1}'.format(stick_count, stick_num))
+
+    # print(fence)
+    print(' '.join(stick_color))
